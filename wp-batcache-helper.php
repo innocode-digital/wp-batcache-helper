c552f9bf9b6a4a844ae4f1beaabeff8647acfd70
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2022-01-28 18:42:54 +0200

    add term and user hooks

--- wp-batcache-helper.php
+++ wp-batcache-helper.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Batcache Helper
  * Description: Improves Batcache cache flushing.
- * Version: 1.0.1
+ * Version: 1.1.0
  * Author: Innocode
  * Author URI: https://innocode.com
  * Tested up to: 5.9.0
@@ -14,7 +14,7 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
 
-// Repeat condition from Batcache Manager.
+// Repeats condition from Batcache Manager.
 if (
     ! isset( $GLOBALS['batcache'] ) ||
     ! is_object( $GLOBALS['batcache'] ) ||
@@ -24,12 +24,9 @@ if (
     return;
 }
 
-if ( ! function_exists( 'innocode_batcache_flush_post' ) ) {
-    function innocode_batcache_flush_post( int $post_id ) : array {
-        return function_exists( 'batcache_clear_url' )
-            ? array_map( 'batcache_clear_url', innocode_get_post_rel_urls( $post_id ) )
-            : [];
-    }
+// Requires Batcache Manager.
+if ( ! function_exists( 'batcache_clear_url' ) ) {
+    return;
 }
 
 if ( ! function_exists( 'innocode_batcache_disable_default' ) ) {
@@ -39,6 +36,16 @@ if ( ! function_exists( 'innocode_batcache_disable_default' ) ) {
 }
 
 add_action( 'plugins_loaded', 'innocode_batcache_disable_default' );
+
+if ( ! function_exists( 'innocode_batcache_flush_post' ) ) {
+    /**
+     * @param int $post_id
+     * @return array
+     */
+    function innocode_batcache_flush_post( int $post_id ) : array {
+        return array_map( 'batcache_clear_url', innocode_get_post_rel_urls( $post_id ) );
+    }
+}
 
 if ( ! function_exists( 'innocode_batcache_handle_post' ) ) {
     function innocode_batcache_handle_post( string $new_status, string $old_status, WP_Post $post ) {
@@ -53,3 +60,27 @@ if ( ! function_exists( 'innocode_batcache_handle_post' ) ) {
 add_action( 'transition_post_status', 'innocode_batcache_handle_post', 10, 3 );
 add_action( 'delete_post', 'innocode_batcache_flush_post' );
 add_action( 'wp_update_comment_count', 'innocode_batcache_flush_post' );
+
+if ( ! function_exists( 'innocode_batcache_flush_term' ) ) {
+    /**
+     * @param int $term_id
+     * @param int $tt_id
+     * @return array
+     */
+    function innocode_batcache_flush_term( int $term_id, int $tt_id ) : array {
+        return array_map( 'batcache_clear_url', innocode_get_term_rel_urls( $tt_id ) );
+    }
+}
+
+add_action( 'saved_term', 'innocode_batcache_flush_term', 10, 2 );
+add_action( 'delete_term', 'innocode_batcache_flush_term', 10, 2 );
+
+if ( ! function_exists( 'innocode_batcache_flush_user' ) ) {
+    function innocode_batcache_flush_user( int $user_id ) : array {
+        return array_map( 'batcache_clear_url', innocode_get_user_rel_urls( $user_id ) );
+    }
+}
+
+add_action( 'personal_options_update', 'innocode_batcache_flush_user' );
+add_action( 'edit_user_profile_update', 'innocode_batcache_flush_user' );
+add_action( 'delete_user', 'innocode_batcache_flush_user' );
